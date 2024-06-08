2edb59c02ce2795e039de50cf82a00fe8e6e047b
Destiny Ugonna Okonkwo <dezok@meta.com>
2024-06-07 18:11:02 -0700

    All key components completed.

---
+++ music-playlist-creator/home.js
@@ -0,0 +1,86 @@
+let playlists = data["playlists"];
+let featured = document.getElementById("featured");
+
+function featuredPlaylist() {
+    // Choose random playlist, create playlist spotlight, and show songs.
+
+    // Choose random playlist index.
+    let randomIdx = Math.floor((Math.random() * (playlists.length)))
+
+    // Playlist information section element creation.
+    let playlistInfo = document.createElement("section");
+    playlistInfo.classList.add("playlist-info");
+
+    // Playlist image element creation.
+    let playlistImg = document.createElement("img");
+    playlistImg.src = playlists[randomIdx]["playlist_art"];
+    playlistImg.classList.add("featured-img");
+
+    // Playlist name element creation.
+    let playlistName = document.createElement("h2");
+    playlistName.innerText = playlists[randomIdx]["playlist_name"];
+
+    // Playlist creator element creation.
+    let playlistCreator = document.createElement("p");
+    playlistCreator.innerText = playlists[randomIdx]["playlist_creator"];
+
+
+    // Creating section for featured songs.
+    let scroll = document.createElement("section");
+    scroll.classList.add("featured-songs");
+
+    // Create scrollable section for songs. To go inside of scroll element.
+    let playlistSongs = document.createElement("section");
+    playlistSongs.classList.add("scrollable");
+
+
+    // Retrieving information for each song.
+    playlists[randomIdx]["songs"].forEach(song => {
+        let playlistSong = document.createElement("span");
+        playlistSong.classList.add("song");
+        playlistSong.id = "featured-song";
+
+        // Song image element creation.
+        let songImg = document.createElement("img");
+        songImg.src = song["cover_art"];
+        songImg.classList.add("featured-song-img");
+
+        // Song info element creation.
+        let songInfo = document.createElement("section");
+        songInfo.classList.add("info");
+
+        // Song name element creation.
+        let songName = document.createElement("h4");
+        songName.innerText = song["title"];
+        songName.classList.add("song-name");
+
+        // Song artist element creation.
+        let songArtist = document.createElement("p");
+        songArtist.innerText = song["artist"];
+
+        // Song album element creation.
+        let songAlbum = document.createElement("p");
+        songAlbum.innerText = song["album"];
+
+        // Assembling song cards.
+        playlistSong.appendChild(songImg);
+        playlistSong.appendChild(songInfo);
+
+        songInfo.appendChild(songName);
+        songInfo.appendChild(songArtist);
+        songInfo.appendChild(songAlbum);
+
+        playlistSongs.appendChild(playlistSong);
+    });
+
+    scroll.appendChild(playlistSongs);
+
+    playlistInfo.appendChild(playlistImg);
+    playlistInfo.appendChild(playlistName)
+    playlistInfo.appendChild(playlistCreator);
+
+    featured.appendChild(playlistInfo);
+    featured.appendChild(scroll);
+}
+
+featuredPlaylist()
--- music-playlist-creator/script.js
+++ music-playlist-creator/script.js
@@ -1,70 +1,107 @@
 let modal = document.getElementById("modal");
 let span = document.getElementsByClassName("close")[0];
+let searchButton = document.getElementById("search-button");
+let searchBar = document.getElementById("searching");
 
 function createPlaylistCards() {
     // Creates the playlist cards.
 
+    // Creation of add playlist card.
+
+    // Playlist card element creation.
+    let addCard = document.createElement("section");
+    addCard.classList.add("playlist-card");
+    addCard.id = "add-card";
+
+    let add = document.createElement("i");
+    add.classList.add("fa-solid");
+    add.classList.add("fa-plus");
+    add.classList.add("fa-5x");
+
+    let createPrompt = document.createElement("p");
+    createPrompt.classList.add("prompt");
+
+    addCard.addEventListener("hover", () => addCard);
+
+    // Assembly of add card using elements.
+    addCard.appendChild(add);
+
+
+    document.getElementById("playlist-cards").appendChild(addCard);
+
     for (let idx = 0; idx < data["playlists"].length;  idx += 1) {
-        // Playlist card element creation.
-        let playlistCard = document.createElement("section");
-        playlistCard.classList.add("playlist-card");
 
-        let playlistInfo = document.createElement("section");
+        console.log(document.getElementById("search-button").value);
+        // If the search bar is not hidden and there is text in the search bar.
+        if ((searchBar.style.display != "none" && (data["playlists"][idx]["playlist_name"].contains(document.getElementById("search-button").value || data["playlists"][idx]["playlist_creator"].contains(document.getElementById("search-button").value)))) || searchBar.style.display == "none") {
+            // Playlist card element creation.
+            let playlistCard = document.createElement("section");
+            playlistCard.classList.add("playlist-card");
 
-        // Playlist image element creation.
-        let playlistImg = document.createElement("img");
-        playlistImg.src = data["playlists"][idx]["playlist_art"];
-        playlistImg.classList.add("playlist-img");
+            let playlistInfo = document.createElement("section");
 
-        // Playlist title element creation.
-        let playlistTitle = document.createElement("h2");
-        playlistTitle.textContent = data["playlists"][idx]["playlist_name"];
+            // Playlist image element creation.
+            let playlistImg = document.createElement("img");
+            playlistImg.src = data["playlists"][idx]["playlist_art"];
+            playlistImg.classList.add("playlist-img");
 
-        // Playlist creator element creation.
-        let playlistCreator = document.createElement("p");
-        playlistCreator.textContent = data["playlists"][idx]["playlist_creator"];
+            // Playlist title element creation.
+            let playlistTitle = document.createElement("h2");
+            playlistTitle.textContent = data["playlists"][idx]["playlist_name"];
+            playlistTitle.classList.add("playlist-title");
 
-        // Playlist likes element creation.
-        let playlistCount = document.createElement("div");
-        playlistCount.classList.add("like-count-info");
+            // Playlist creator element creation.
+            let playlistCreator = document.createElement("p");
+            playlistCreator.textContent = data["playlists"][idx]["playlist_creator"];
 
-        let likeCount = document.createElement("p");
-        likeCount.textContent = data["playlists"][idx]["likeCount"];
+            // Playlist likes element creation.
+            let playlistCount = document.createElement("div");
+            playlistCount.classList.add("like-count-info");
 
-        let heart = document.createElement("i");
-        heart.classList.add("fa-regular");
-        heart.classList.add("fa-heart");
+            let likeCount = document.createElement("p");
+            likeCount.textContent = data["playlists"][idx]["likeCount"];
 
-        console.log(heart)
+            let heart = document.createElement("i");
+            heart.classList.add("fa-regular");
+            heart.classList.add("fa-heart");
 
-        // Assembly of card using elements.
-        playlistCard.appendChild(playlistInfo);
+            let trash = document.createElement("i");
+            trash.classList.add("fa-solid");
+            trash.classList.add("fa-trash");
 
-        playlistInfo.appendChild(playlistImg);
-        playlistInfo.appendChild(playlistTitle);
-        playlistInfo.appendChild(playlistCreator);
+            // Assembly of card using elements.
+            playlistCard.appendChild(playlistInfo);
 
-        playlistCard.appendChild(playlistCount);
+            playlistInfo.appendChild(playlistImg);
+            playlistInfo.appendChild(playlistTitle);
+            playlistInfo.appendChild(playlistCreator);
 
-        playlistCount.appendChild(heart);
-        playlistCount.appendChild(likeCount);
+            playlistCard.appendChild(playlistCount);
 
+            playlistCount.appendChild(heart);
+            playlistCount.appendChild(likeCount);
+            playlistCount.appendChild(trash);
 
-        // Adding event listner to each playlist card.
-        heart.addEventListener("click", () => likePlaylist(heart, likeCount))
-        playlistInfo.addEventListener("click", () => openModal(data["playlists"][idx]));
+            // Adding event listner to each playlist card.
+            heart.addEventListener("click", () => likePlaylist(heart, likeCount));
+            trash.addEventListener("click", () => deletePlaylist(playlistCard))
+            playlistInfo.addEventListener("click", () => openModal(data["playlists"][idx]));
 
-        document.getElementById("playlist-cards").appendChild(playlistCard);
+            document.getElementById("playlist-cards").appendChild(playlistCard);
+        }
     }
 }
 
+function deletePlaylist(playlistCard) {
+    document.getElementById("playlist-cards").removeChild(playlistCard);
+}
 
 function openModal(playlist) {
     // Opens the modal and populates it with data from the playlist.
     // Why doesn't this wok when we just pass playlist?
     document.getElementById("playlist-name").innerText = playlist["playlist_name"];
     document.getElementById("playlist-img").src = playlist["playlist_art"];
-    document.getElementById("creator-name").innerHTML = `<strong>Artist: </strong> ${playlist["playlist_creator"]}`;
+    document.getElementById("creator-name").innerHTML = `<strong>Creator: </strong> ${playlist["playlist_creator"]}`;
 
     document.getElementById("shuffle-button").addEventListener("click", () => shuffleSongs(playlist));
 
@@ -73,13 +110,14 @@ function openModal(playlist) {
     modal.style.display = "block";
 }
 
-function likePlaylist(heart, likeCount) {
+function likePlaylit(heart, likeCount) {
     // Changing the heart to solid
     if (String(heart.classList).includes("fa-regular")) {
         // Change to solid if liked.
         heart.classList = ""
         heart.classList.add("fa-solid");
         heart.classList.add("fa-heart");
+        heart.classList.add("liked-heart");
 
         likeCount.innerText = String(Number(likeCount.innerText) + 1);
     }
@@ -116,15 +154,23 @@ function createSongList(playlist) {
         // Song title element creation.
         let songTitle = document.createElement("h2");
         songTitle.textContent = playlist["songs"][idx]["title"];
-        songCard.classList.add("songName");
+        songCard.classList.add("song-name");
 
         // Song creator element creation.
         let songArtist = document.createElement("p");
         songArtist.textContent = playlist["songs"][idx]["artist"];
+        songArtist.classList.add("artist-name");
 
         // Song album element creation.
         let songAlbum = document.createElement("p");
         songAlbum.textContent = playlist["songs"][idx]["album"];
+
+        // Song play button element creation.
+        let play = document.createElement("i");
+        play.classList.add("fa-solid");
+        play.classList.add("fa-play");
+        play.classList.add("fa-4x");
+        play.classList.add("play-button");
 
         // Song duration element creation.
         let songDuration = document.createElement("p");
@@ -140,6 +186,8 @@ function createSongList(playlist) {
         infoContainer.appendChild(songAlbum);
 
         songCard.appendChild(songDuration);
+        songCard.appendChild(play);
+
 
         document.getElementById("songs").appendChild(songCard);
     }
@@ -158,6 +206,21 @@ function shuffleSongs(playlist) {
     }
 
     createSongList(playlist);
+}
+
+searchButton.onclick = function() {
+    // Pop up search bar when search button is clicked and close when clicked again.
+    if (searchBar.style.display == "none") {
+        searchBar.style.display = "block";
+    }
+    else {
+        searchBar.style.display = "none";
+    }
+}
+
+function searching () {
+    // Re-rendering search bar on input of new text.
+    createPlaylistCards();
 }
 
 span.onclick = function() {
